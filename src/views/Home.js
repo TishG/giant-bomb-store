375119ca166210c9ca9e64893ee4f6f233e386de
Tish <latisha.griffiths@programmer.net>
2019-12-16 17:19:17 -0600

    add header

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Home.css";
 
+//components
+import Header from '../components/Header';
+
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <Header />
     </div>
   );
 };
